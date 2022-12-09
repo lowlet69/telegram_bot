cc55f05646ecca6f30c99a21ac1cfbdf286baa66
lowlet69 <89310228+lowlet69@users.noreply.github.com>
2022-12-09 12:42:49 +0300

    first_upload

---
+++ config.py
@@ -0,0 +1,14 @@
+TOKEN = '5505809133:AAFDyucnPHooYai_zpLhjO_iHIjEfoqajCs'
+
+keys = {
+    'биткоин': 'BTC',
+    'BTC': 'BTC',
+    'эфириум': 'ETH',
+    'ETH': 'ETH',
+    'доллар': 'USD',
+    'USD': 'USD',
+    'евро': 'EUR',
+    'EUR': 'EUR',
+    'рубль': 'RUB',
+    'RUB': 'RUB'
+}
